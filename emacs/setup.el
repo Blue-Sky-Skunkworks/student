75fab15f9048b42d2ef091be00e511e4cc1f8527
conrad <whalliburton@gmail.com>
2016-03-31 16:43:24 -0600

    setup

---
+++ emacs/setup.el
@@ -0,0 +1,9 @@
+;; Missoula Civic Hackathon student emacs setup
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
