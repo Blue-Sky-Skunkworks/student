67c18152c59049420e32dc88c17050dcfb498018
William Halliburton <will@blueskystewardship.org>
2016-04-07 21:39:01 +0000

    add some emacs packages

--- emacs/setup.el
+++ emacs/setup.el
@@ -3,7 +3,19 @@
 (defvar *student-emacs-packages*
   '(use-package
      evil
-     paredit))
+     paredit
+     igrep
+     ace-jump-mode
+     sr-speedbar
+     command-log-mode
+     evil-leader
+     evil-org
+     evil-cleverparens
+     mic-paren
+     vline
+     auto-complete
+     ac-slime
+     scss-mode))
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
